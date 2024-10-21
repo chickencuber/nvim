f95902a98f13c8bb97e70748a603a977e396498f
chickencuber <n1batman728@gmail.com>
2024-10-21 18:04:49 -0500

    did thing

--- lua/main/packer.lua
+++ lua/main/packer.lua
@@ -4,38 +4,38 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-	-- Packer can manage itself
-	use 'wbthomason/packer.nvim'
-	use {
-		'nvim-telescope/telescope.nvim', tag = '0.1.8',
-		-- or                            , branch = '0.1.x',
-		requires = { {'nvim-lua/plenary.nvim'} }
-	}
-	use { "catppuccin/nvim", as = "catppuccin",config= function()
-		vim.cmd.colorscheme "catppuccin-mocha"
-	end
-	}
-	require("catppuccin").setup({
-		mason = true
-	})
-	use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
-	use('theprimeagen/harpoon')
-	use('mbbill/undotree')
-	use('tpope/vim-fugitive')
-	use {'VonHeikemen/lsp-zero.nvim',
-		{'neovim/nvim-lspconfig'},
-		{'williamboman/mason.nvim'},
-        {'williamboman/mason-lspconfig.nvim'},
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.8',
+        -- or                            , branch = '0.1.x',
+        requires = { {'nvim-lua/plenary.nvim'} }
+    }
+    use { "catppuccin/nvim", as = "catppuccin",config= function()
+        vim.cmd.colorscheme "catppuccin-mocha"
+    end
+}
+require("catppuccin").setup({
+    mason = true
+})
+use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+use('theprimeagen/harpoon')
+use('mbbill/undotree')
+use('tpope/vim-fugitive')
+use {'VonHeikemen/lsp-zero.nvim',
+{'neovim/nvim-lspconfig'},
+{'williamboman/mason.nvim'},
+{'williamboman/mason-lspconfig.nvim'},
 
-        {'hrsh7th/nvim-cmp'},
-        {'hrsh7th/cmp-buffer'},
-        {'hrsh7th/cmp-path'},
-        {'saadparwaiz1/cmp_luasnip'},
-        {'hrsh7th/cmp-nvim-lsp'},
-        {'hrsh7th/cmp-nvim-lua'},
+{'hrsh7th/nvim-cmp'},
+{'hrsh7th/cmp-buffer'},
+{'hrsh7th/cmp-path'},
+{'saadparwaiz1/cmp_luasnip'},
+{'hrsh7th/cmp-nvim-lsp'},
+{'hrsh7th/cmp-nvim-lua'},
 
-        {'L3MON4D3/LuaSnip'},
-        {'rafamadriz/friendly-snippets'},
+{'L3MON4D3/LuaSnip'},
+{'rafamadriz/friendly-snippets'},
     }
     use {'akinsho/nvim-bufferline.lua'}
     use {
