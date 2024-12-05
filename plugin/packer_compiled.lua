1bdbc5ded054a2661a9a46723969d0de16b77de7
chickencuber <n1batman728@gmail.com>
2024-12-05 15:36:54 -0600

    did thing

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -171,7 +171,7 @@ _G.packer_plugins = {
     url = "https://github.com/hrsh7th/nvim-cmp"
   },
   ["nvim-colorizer.lua"] = {
-    config = { "\27LJ\2\nð\2\0\0\6\0\r\0\0176\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\4\0005\3\3\0=\3\5\0025\3\6\0005\4\a\0005\5\b\0=\5\t\4=\4\n\3=\3\v\0024\3\0\0=\3\f\2B\0\2\1K\0\1\0\rbuftypes\25user_default_options\tsass\fparsers\1\2\0\0\bcss\1\0\2\venable\1\fparsers\0\1\0\14\vhsl_fn\1\rRRGGBBAA\1\16virtualtext\bâ– \rtailwind\1\18always_update\1\vRRGGBB\2\rAARRGGBB\1\vrgb_fn\1\bRGB\2\vcss_fn\1\nnames\2\bcss\1\tmode\16virtualtext\tsass\0\14filetypes\1\0\3\14filetypes\0\25user_default_options\0\rbuftypes\0\1\2\0\0\6*\nsetup\14colorizer\frequire\0" },
+    config = { "\27LJ\2\nð\2\0\0\6\0\r\0\0176\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\4\0005\3\3\0=\3\5\0025\3\6\0005\4\a\0005\5\b\0=\5\t\4=\4\n\3=\3\v\0024\3\0\0=\3\f\2B\0\2\1K\0\1\0\rbuftypes\25user_default_options\tsass\fparsers\1\2\0\0\bcss\1\0\2\venable\1\fparsers\0\1\0\14\rRRGGBBAA\1\vhsl_fn\1\bRGB\2\bcss\1\tmode\16virtualtext\tsass\0\vcss_fn\1\nnames\2\vrgb_fn\1\rAARRGGBB\1\vRRGGBB\2\18always_update\1\rtailwind\1\16virtualtext\bâ– \14filetypes\1\0\3\14filetypes\0\25user_default_options\0\rbuftypes\0\1\2\0\0\6*\nsetup\14colorizer\frequire\0" },
     loaded = true,
     path = "/home/chickencuber/.local/share/nvim/site/pack/packer/start/nvim-colorizer.lua",
     url = "https://github.com/NvChad/nvim-colorizer.lua"
@@ -229,9 +229,13 @@ _G.packer_plugins = {
 }
 
 time([[Defining packer_plugins]], false)
+-- Config for: better-comments.nvim
+time([[Config for better-comments.nvim]], true)
+try_loadstring("\27LJ\2\n<\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nSetup\19better-comment\frequire\0", "config", "better-comments.nvim")
+time([[Config for better-comments.nvim]], false)
 -- Config for: nvim-colorizer.lua
 time([[Config for nvim-colorizer.lua]], true)
-try_loadstring("\27LJ\2\nð\2\0\0\6\0\r\0\0176\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\4\0005\3\3\0=\3\5\0025\3\6\0005\4\a\0005\5\b\0=\5\t\4=\4\n\3=\3\v\0024\3\0\0=\3\f\2B\0\2\1K\0\1\0\rbuftypes\25user_default_options\tsass\fparsers\1\2\0\0\bcss\1\0\2\venable\1\fparsers\0\1\0\14\vhsl_fn\1\rRRGGBBAA\1\16virtualtext\bâ– \rtailwind\1\18always_update\1\vRRGGBB\2\rAARRGGBB\1\vrgb_fn\1\bRGB\2\vcss_fn\1\nnames\2\bcss\1\tmode\16virtualtext\tsass\0\14filetypes\1\0\3\14filetypes\0\25user_default_options\0\rbuftypes\0\1\2\0\0\6*\nsetup\14colorizer\frequire\0", "config", "nvim-colorizer.lua")
+try_loadstring("\27LJ\2\nð\2\0\0\6\0\r\0\0176\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\4\0005\3\3\0=\3\5\0025\3\6\0005\4\a\0005\5\b\0=\5\t\4=\4\n\3=\3\v\0024\3\0\0=\3\f\2B\0\2\1K\0\1\0\rbuftypes\25user_default_options\tsass\fparsers\1\2\0\0\bcss\1\0\2\venable\1\fparsers\0\1\0\14\rRRGGBBAA\1\vhsl_fn\1\bRGB\2\bcss\1\tmode\16virtualtext\tsass\0\vcss_fn\1\nnames\2\vrgb_fn\1\rAARRGGBB\1\vRRGGBB\2\18always_update\1\rtailwind\1\16virtualtext\bâ– \14filetypes\1\0\3\14filetypes\0\25user_default_options\0\rbuftypes\0\1\2\0\0\6*\nsetup\14colorizer\frequire\0", "config", "nvim-colorizer.lua")
 time([[Config for nvim-colorizer.lua]], false)
 -- Config for: catppuccin
 time([[Config for catppuccin]], true)
@@ -241,10 +245,6 @@ time([[Config for catppuccin]], false)
 time([[Config for indent-blankline.nvim]], true)
 try_loadstring("\27LJ\2\nY\0\0\4\0\6\0\t6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\4\0005\3\3\0=\3\5\2B\0\2\1K\0\1\0\nscope\1\0\1\nscope\0\1\0\1\fenabled\1\nsetup\bibl\frequire\0", "config", "indent-blankline.nvim")
 time([[Config for indent-blankline.nvim]], false)
--- Config for: better-comments.nvim
-time([[Config for better-comments.nvim]], true)
-try_loadstring("\27LJ\2\n<\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nSetup\19better-comment\frequire\0", "config", "better-comments.nvim")
-time([[Config for better-comments.nvim]], false)
 vim.cmd [[augroup packer_load_aucmds]]
 vim.cmd [[au!]]
   -- Event lazy-loads
