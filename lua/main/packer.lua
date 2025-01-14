1111586f010e7e8fa840196992294d9848f378db
chickencuber <n1batman728@gmail.com>
2025-01-13 18:59:41 -0600

    fixed thing

--- lua/main/packer.lua
+++ lua/main/packer.lua
@@ -110,9 +110,9 @@ use {'VonHeikemen/lsp-zero.nvim',
             end
         }
         use {
-                "wstucco/c3.nvim",
-                config = function()
-                    require("c3")
-                end,
-            }
+            "wstucco/c3.nvim",
+            config = function()
+                require("c3")
+            end,
+        }
     end)
