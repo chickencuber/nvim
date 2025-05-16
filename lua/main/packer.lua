7ed8ad2cbbeec7a9fefc02cd3518a5334d2456be
chickencuber <n1batman728@gmail.com>
2025-05-15 22:59:08 -0500

    switched from harpoon to harpoon 2

--- lua/main/packer.lua
+++ lua/main/packer.lua
@@ -29,7 +29,11 @@ require("catppuccin").setup({
 use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
 use('nvim-treesitter/nvim-treesitter-context')
 use('nvim-treesitter/nvim-treesitter-refactor')
-use('theprimeagen/harpoon')
+use({
+    'theprimeagen/harpoon',
+    branch = "harpoon2",
+    requires = { {"nvim-lua/plenary.nvim"} }
+})
 use('mbbill/undotree')
 -- use('tpope/vim-fugitive')
 use { 'TimUntersberger/neogit', requires = 'nvim-lua/plenary.nvim' }
