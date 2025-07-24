f67c54c58afcc59ecd7153f1c47e11cc14973318
chickencuber <n1batman728@gmail.com>
2025-07-24 01:13:53 -0500

    did thing

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -1,5 +1,5 @@
 require('lualine').setup{
-    options = { section_separators = '', component_separators = '' },
+    -- options = { section_separators = '', component_separators = '' },
     sections = {
         lualine_a = {'mode'},
         lualine_b = {'branch', 'diff', 'diagnostics'},
