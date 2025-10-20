a98d64c1dfd2874e9f584b9652730a424a1c2bfa
chickencuber <n1batman728@gmail.com>
2025-10-20 08:18:52 -0500

    change orgmode config

--- lua/main/plugins/orgmode.lua
+++ lua/main/plugins/orgmode.lua
@@ -5,6 +5,7 @@ return {
     config = function()
         require('orgmode').setup({
             org_agenda_files = '~/orgfiles/**/*',
+            org_startup_indented = true,
             org_default_notes_file = '~/orgfiles/refile.org',
             org_startup_folded = "inherit",
             mappings = {
